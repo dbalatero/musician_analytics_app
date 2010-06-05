c9dba9db997ee87bce2498e9885387d6f2af96cd
David Balatero <dbalatero@gmail.com>
2010-06-05 02:08:03 -0700

    Added stats table view.

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -2,5 +2,6 @@ class StatsController < ApplicationController
   before_filter :require_user
 
   def index
+    @stats = Stat.most_recent
   end
 end
--- app/models/stat.rb
+++ app/models/stat.rb
@@ -1,2 +1,3 @@
 class Stat < ActiveRecord::Base
+  named_scope :most_recent, :order => 'created_at desc'
 end
