670c6fc2ac3e9e3f4b9ccdd49102164c95dd0774
Bantik <corey@seologic.com>
2010-09-24 20:18:23 -0500

    Added graphing via Seer; created a simple and clean UI.

--- app/models/stat.rb
+++ app/models/stat.rb
@@ -1,3 +1,28 @@
 class Stat < ActiveRecord::Base
-  named_scope :most_recent, :order => 'created_at desc'
+
+  named_scope :all, :order => 'created_at asc'
+
+  def date
+    self.created_at.strftime("%m/%d/%Y")
+  end
+
+  def datum(kind)
+    case kind
+      when :lastfm_listeners;      Datum.new(:name => 'Last.fm Listeners', :date => self.date, :count => send(kind).to_i)
+      when :lastfm_playcount;      Datum.new(:name => 'Last.fm Playcount', :date => self.date, :count => send(kind).to_i)
+      when :twitter_friends;       Datum.new(:name => 'Twitter Friends',   :date => self.date, :count => send(kind).to_i)
+      when :twitter_followers;     Datum.new(:name => 'Twitter Followers', :date => self.date, :count => send(kind).to_i)
+      when :myspace_profile_views; Datum.new(:name => 'MySpace Views',     :date => self.date, :count => send(kind).to_i)
+    end
+  end
+  
+  class Datum
+    attr_accessor :name
+    attr_accessor :date
+    attr_accessor :count
+    def initialize(args)
+      [:name, :date, :count].each{|a| self.send("#{a}=", args[a]) if args[a]}
+    end
+  end
+  
 end
